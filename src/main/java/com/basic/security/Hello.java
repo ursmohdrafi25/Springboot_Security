f2f0190fa3ae94b38f16f839c90bdf402fe8e588
ursmohdrafi25 <rafimohd.sh@yahoo.com>
2022-11-19 22:25:43 +0530

    first commit

---
+++ src/main/java/com/basic/security/Hello.java
@@ -0,0 +1,13 @@
+package com.basic.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hello {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hello.class, args);
+	}
+
+}
